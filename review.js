ee919345fbb56afac54760f42058902695377201
Aniket Dey <meaniket1025@gmail.com>
2025-02-19 16:09:58 +0530

    handled some edge cases

--- review.js
+++ review.js
@@ -7,22 +7,24 @@ btn.addEventListener('click', ()=>{
     const name = inname.value;
     const desc = indesc.value;
     
+    if (name && desc) {
+        console.log("hi");
+        
+        let ndiv = document.createElement('div')
+        ndiv.classList.add("post");
+        ndiv.innerHTML =''
+        console.log(ndiv);
     
-    let ndiv = document.createElement('div')
-    ndiv.classList.add("post");
-    ndiv.innerHTML =''
-    console.log(ndiv);
-
-    const h1 = document.createElement('h1')
-    h1.innerText = name;
-
-    const p = document.createElement('p')
-    p.innerText = desc;
-
+        const h1 = document.createElement('h1')
+        h1.innerText = name;
     
-    ndiv.appendChild(h1)
-    ndiv.appendChild(p)
-    // postarea.appendChild(ndiv) it's appen the element in last position
-    postarea.prepend(ndiv) // it add the element in the frist position
-    document.getElementById('form').reset();
+        const p = document.createElement('p')
+        p.innerText = desc;
+    
+        
+        ndiv.appendChild(h1)
+        ndiv.appendChild(p) // postarea.appendChild(ndiv) it's appen the element in last position
+        postarea.prepend(ndiv) // it add the element in the frist position
+        document.getElementById('form').reset();
+    }
 })
